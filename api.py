f7e52446b4abf764f536eb7b959723bd1df62535
AlirezaSM <sedighi_a77@yahoo.com>
2024-04-15 10:15:28 +0330

    Bugfix: video_file address mistake + appropriate response returned

--- api.py
+++ api.py
@@ -1,26 +1,27 @@
 import os
+import uvicorn
 import subprocess
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 # Define the FastAPI app
 app = FastAPI()
 
 # Define the route to execute the bash command
 @app.post("/track")
-async def track(video_file: str):
-    database_path = '/paddle'
-    video_path = os.path.join(database_path, 'partition_videos', '_'.join(video_file.split('_')[:-1])+'_total', video_file)
-    print(video_path)
+def track(video_file: str):
+    database_path = '/database'
+    video_path = os.path.join(database_path, 'partition_videos', '_'.join(video_file.split('_')[:-1]) + '_total', video_file)
     command = f"python deploy/pipeline/pipeline.py --config deploy/pipeline/config/infer_cfg_pphuman.yml " \
-              f"--video_file={os.path.join(database_path, 'partition_videos', video_file)} --device=gpu"
+              f"--video_file={video_path} --device=gpu"
     try:
         # Execute the command
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return {"output": result.stdout, "error": result.stderr}
+        return JSONResponse({"output": result.stdout,
+                             "error": result.stderr})
     except Exception as e:
-        return {"error": str(e)}
+        return JSONResponse({"error": str(e)}, status_code=500)
 
 # Run the FastAPI app with uvicorn
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8060)
